e351fec2f6411d1564b7114e4d317223d31e9883
jiajia.zhou <jiajia.zhou@thoughtworks.com>
2023-06-22 18:37:17 +0800

    chore: init style

---
+++ src/views/FrameView.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import Logo from '../assets/icons/logo.svg'
+export default {
+  data() {
+    return {
+      Logo
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+    <div><img alt="logo" :src="Logo" /></div>
+  </header>
+</template>
